e874b45151540e57bc45f7ff29896002a6d0ce19
shabdan <kozhonalievshabdan@gmail.com>
2023-10-27 22:56:20 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+// bg-gray-800 - dark
+// bg-teal-500 - dark teal
+// bg-teal-400 - light teal
+// bg-blue-100 gray
+
+function App() {
+  return (
+    <div className="App">
+      <p className=" bg-blue-100">Something</p>
+      <h1>same</h1>
+    </div>
+  )
+}
+
+export default App;
