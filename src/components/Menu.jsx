2830c1c9a214f06ebebc6446cd8d8ef6f17388eb
shabdan <kozhonalievshabdan@gmail.com>
2023-10-29 01:14:52 +0600

    added links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <div className="menu w-44 fixed bg-teal-400 h-screen">
-      <p>Menu</p>
-      <p>Dashboard</p>
+    <div className="menu w-44 fixed bg-teal-400 h-screen px-2 flex flex-col justify-center items-center">
+      <Link to='/login' className=" text-white font-semibold">Login</Link>
+      <Link to="/signup" className=" text-white font-semibold">Signup</Link>
+      <Link to="/dashboard" className=" text-white font-semibold">Dashboard</Link>
     </div>
-  )
+  );
 };
 
 export default Menu;
